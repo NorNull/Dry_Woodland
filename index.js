800ee3ed93f482cff13deda5d953052fd9630bcd
UnitySam Rung <unitysamrung@gmail.com>
2016-07-24 00:09:26 +0700

    change

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ var ai_room = [];
 
 io.on ('connection', function (socket) {
   socket.on ('Born', function (r_data) {
+      console.log ('test');
+
       for (var index = 0; index < user.length; index++) {
         if (user [index, 'room'] == r_data ['room']) {
           var data = {user : user [index, 'user'], cha : user [index, 'cha'],
